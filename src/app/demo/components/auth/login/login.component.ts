9a419faff570b0c54c03fe9cfb33f4ab70a0a921
Ayesha khan <ayeshaakhan138@gmail.com>
2024-05-04 00:00:02 +0530

    work on landing page ,set routing of landing,login page

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
@@ -6,10 +8,25 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 })
 export class LoginComponent {
     rememberMe: boolean = false;
+    loginForm: FormGroup;
 
-    constructor(private layoutService: LayoutService) {}
+    constructor(private layoutService: LayoutService, private fb: FormBuilder,
+        private router: Router
+    ) {
+        this.loginForm = this.fb.group({
+            username: [null, [Validators.required]],
+            password: [null, Validators.required],
+        });
+    }
 
     get dark(): boolean {
         return this.layoutService.config().colorScheme !== 'light';
+    }
+
+    // login
+    login(value: any) {
+     console.log("jbdsbdmbsdm");
+     
+        this.router.navigateByUrl('/dashboard');
     }
 }
